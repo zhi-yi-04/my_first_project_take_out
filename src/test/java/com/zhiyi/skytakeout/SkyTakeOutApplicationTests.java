f14539400f2a83e90ae87c073ce5bb44ce585ee5
zhi-yi-04 <zhiyi15250@163.com>
2026-07-23 15:54:45 +0800

    Initial commit: 创建项目基础结构

---
+++ src/test/java/com/zhiyi/skytakeout/SkyTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiyi.skytakeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
